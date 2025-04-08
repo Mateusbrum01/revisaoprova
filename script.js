8937e85d8af74d26ddb9ab9f018a34b435b1cd9f
Mateusbrum01 <mateus_cagneti@estudante.sesisenai.org.br>
2025-04-08 02:00:22 +0000

    Add - atv de revisão

---
+++ script.js
@@ -0,0 +1,11 @@
+function pedirInformacoes() {
+    alert("Bem-vindo ao site de revisão para a prova!");
+    let nome = prompt("Por favor, informe seu nome:");
+    let email = prompt("Agora, informe seu principal email:");
+    if (nome && email) {
+        alert(`Obrigado, ${nome}! Recebemos suas informações com sucesso. Bem-vindo!`);
+    } else {
+        alert("Você não preencheu todas as informações, tente novamente.");
+    }
+}
+window.onload = pedirInformacoes;
